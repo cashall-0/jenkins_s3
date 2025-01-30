144735c33b2a2c14d66da97257ee4d20564fbad8
siyaka promise <siyakapromise@gmail.com>
2025-01-30 13:54:57 +0100

    change bucket name

--- main.tf
+++ main.tf
@@ -1,9 +1,9 @@
 provider "aws" {
   region = "ap-south-1"
 }
-resource "aws_s3_bucket" "bucket" {
-  bucket  = "my-jenkins-s3bucket"
+resource "aws_s3_bucket" "b" {
+  bucket  = "my-new-jenkins-s3bucket"
   tags    = {
-	Name          = "MyJenkinsS3Bucket"
+	Name          = "MyJenkinsS3Bucketnew"
   }
 }
